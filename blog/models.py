71f532ebd267e5233ca0cf1117bd2acda28d1bf1
Tonisha Baker <bakertoni17gmail.com>
2019-09-12 21:49:23 +0200

    Add feature to create new blog posts from frontend.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
@@ -12,7 +13,6 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
-
 
     def __str__(self):
         return self.title
